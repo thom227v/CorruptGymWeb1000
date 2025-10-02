c5b68235027c22dc08513aab6c0f8b0ea60a2fb6
thom227v <61274@edu.sde.dk>
2025-10-02 12:00:47 +0200

    Add project files.

---
+++ ClassLibrary1/Interfaces/ISqlDataAccess.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Interfaces
+{
+    public interface ISqlDataAccess
+    {
+        Task<IEnumerable<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionId = "DefaultConnection");
+        Task SaveData<T>(string storedProcedure, T parameters, string connectionId = "DefaultConnection");
+    }
+}
---
+++ ClassLibrary1/SqlDataAccess/SqlDataAccess.cs
@@ -0,0 +1,50 @@
+﻿using ClassLibrary1.Interfaces;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.SqlDataAccess
+{
+    public class SqlDataAccess : ISqlDataAccess
+    {
+        private readonly IConfiguration _configuration;
+
+        public SqlDataAccess(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<IEnumerable<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionId = "DefaultConnection")
+        {
+            string connectionString = _configuration.GetConnectionString(connectionId);
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                var data = await connection.QueryAsync<T>(
+                    storedProcedure,
+                    parameters,
+                    commandType: CommandType.StoredProcedure);
+                return data;
+            }
+        }
+
+
+        public async Task SaveData<T>(string storedProcedure, T parameters, string connectionId = "DefaultConnection")
+        {
+            string connectionString = _configuration.GetConnectionString(connectionId);
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                await connection.ExecuteAsync(
+                    storedProcedure,
+                    parameters,
+                    commandType: CommandType.StoredProcedure);
+            }
+        }
+    }
+}
